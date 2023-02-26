6bae13b92a3197713c412a8e84eb033c941bdcf0
rjgallac <rjgallac@hotmail.com>
2023-02-26 15:24:59 +0000

    Initial commit

---
+++ test/lambda-github.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const LambdaGithub = require('../lib/lambda-github-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-github-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new LambdaGithub.LambdaGithubStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

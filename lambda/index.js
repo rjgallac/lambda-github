9c7df82d2ed36fdc02ee3571db871ae3c199191c
rjgallac <rjgallac@hotmail.com>
2023-02-27 20:47:33 +0000

    test update

--- lambda/index.js
+++ lambda/index.js
@@ -1,4 +1,4 @@
 exports.handler =  async function(event, context) {
-    console.log("EVENT2: \n" + JSON.stringify(event, null, 2))
+    console.log("EVENT3: \n" + JSON.stringify(event, null, 2))
     return context.logStreamName
   }
